d59e9c1fe77dd26ac8b8206567bb0dbe3db0e511
ali1373107 <113782230+ali1373107@users.noreply.github.com>
2022-09-28 21:51:45 +0100

    Update script.js
    ddd

--- script.js
+++ script.js
@@ -64,27 +64,6 @@ test("There are at least 500 words on the page", () => {
   expect(getWords.length).toBeGreaterThanOrEqual(500);
 });
 
-var images;
-
-
-images = ['https://tse4.mm.bing.net/th?id=OIP.eIEz73xZoLmYwqTF92FciwHaEw&pid=Api&P=0', 'https://tse3.mm.bing.net/th?id=OIP.cFbSMhjRMOmNaJsl4nI2fwHaF7&pid=Api&P=0', 'https://tse2.mm.bing.net/th?id=OIP.ez64pUNihhf5d7vgHgdn8wHaFj&pid=Api&P=0', 'https://tse2.mm.bing.net/th?id=OIP.OlugQrXt4ksrPC-j0mCHMQHaFj&pid=Api&P=0'];
-let element_img = document.getElementById('img');
-element_img.setAttribute("src", images[0]);
-
-
-document.getElementById('pre').addEventListener('click', (event) => {
-  let element_img2 = document.getElementById('img');
-  images.unshift(images.slice(-1)[0]);
-  element_img2.setAttribute("src", images.pop());
-
-});
-
-document.getElementById('next').addEventListener('click', (event) => {
-  let element_img3 = document.getElementById('img');
-  images.push(images[0]);
-  element_img3.setAttribute("src", images.shift());
-
-});
 var array;
 
 function getNumberOrString(value) {
@@ -116,4 +95,25 @@ array = [];
 const console = document.getElementById("tests");
 prettify.toHTML(run(), console);
 
+var images;
+
+
+images = ['https://tse4.mm.bing.net/th?id=OIP.eIEz73xZoLmYwqTF92FciwHaEw&pid=Api&P=0', 'https://tse3.mm.bing.net/th?id=OIP.cFbSMhjRMOmNaJsl4nI2fwHaF7&pid=Api&P=0', 'https://tse2.mm.bing.net/th?id=OIP.ez64pUNihhf5d7vgHgdn8wHaFj&pid=Api&P=0', 'https://tse2.mm.bing.net/th?id=OIP.OlugQrXt4ksrPC-j0mCHMQHaFj&pid=Api&P=0'];
+let element_img = document.getElementById('img');
+element_img.setAttribute("src", images[0]);
+
+
+document.getElementById('pre').addEventListener('click', (event) => {
+  let element_img2 = document.getElementById('img');
+  images.unshift(images.slice(-1)[0]);
+  element_img2.setAttribute("src", images.pop());
+
+});
+
+document.getElementById('next').addEventListener('click', (event) => {
+  let element_img3 = document.getElementById('img');
+  images.push(images[0]);
+  element_img3.setAttribute("src", images.shift());
+
+});
 
