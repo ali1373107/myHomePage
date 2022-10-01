7089815ef30ee9ae1a8d102e3b51f6a56994c25c
ali1373107 <113782230+ali1373107@users.noreply.github.com>
2022-10-01 02:41:45 +0100

    Update script.js
    rhfg

--- script.js
+++ script.js
@@ -102,3 +102,20 @@ document.getElementById('secendbtnprice').addEventListener('click', (event) => {
   element_total_price5.innerText = totalprice.reduce((a,b) => a+b, 0);
 
 });
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
